fa9ccca8a172f967d68c653e4902cca339862b11
poresky <chrisporesky@berkeley.edu>
2019-03-22 10:08:02 -0700

    Added commands to find and read all nodes in MATLAB OPC UA Client

--- OPC_UA_Client.m
+++ OPC_UA_Client.m
@@ -1,6 +1,35 @@
+% Define client and connect to the Main Server
 uaClient = opcua('192.168.1.3',49581);
 connect(uaClient)
-TempNode = findNodeByName(uaClient.Namespace,'Message');
+
+% Obtain a list of available nodes on the server
+uaClient.Namespace
+
+% Define nodes in MATLAB
+ChatNode = findNodeByName(uaClient.Namespace,'Chat');
+BoPDataNode = findNodeByName(uaClient.Namespace,'BoP Data');
+DAQDataNode = findNodeByName(uaClient.Namespace,'DAQ Data');
+CIETDataNode = findNodeByName(uaClient.Namespace,'CIET Data');
+BoPInputsNode = findNodeByName(uaClient.Namespace,'BoP Inputs');
+SpoofDataNode = findNodeByName(uaClient.Namespace,'Spoof Data');
+BoPOutputsNode = findNodeByName(uaClient.Namespace,'BoP Outputs');
+ResultDataNode = findNodeByName(uaClient.Namespace,'Result Data');
+ResultCIETDataNode = findNodeByName(uaClient.Namespace,'Result CIET Data');
+SpoofedCIETDataNode = findNodeByName(uaClient.Namespace,'Spoofed CIET Data');
+BooleanControlNode = findNodeByName(uaClient.Namespace,'Boolean Control Actions');
+NumericControlNode = findNodeByName(uaClient.Namespace,'Numeric Control Actions');
+ResultBooleanControlNode = findNodeByName(uaClient.Namespace,'Result Boolean Controls');
+ResultNumericControlNode = findNodeByName(uaClient.Namespace,'Result Numeric Controls');
+SpoofedBooleanControlNode = findNodeByName(uaClient.Namespace,'Spoofed Boolean Controls');
+SpoofedNumericControlNode = findNodeByName(uaClient.Namespace,'Spoofed Numeric Controls');
+
+% Read node values
+readValue(uaClient,CIETDataNode)
+
+% Write node values
+writeValue(uaClient,CIETDataNode,[])
+
+DataNode = findNodeByName(uaClient.Namespace,'Message');
 readValue(uaClient,TempNode)
 writeValue(uaClient,TempNode,55);
 tic;
@@ -23,3 +52,16 @@ uaClient.Namespace
 chatNode = findNodeByName(uaClient.Namespace, 'Chat')
 readValue(uaClient, chatNode)
 value = readValue(uaClient, chatNode)
+
+figure(1)
+ResultCIETData = [];
+for i = 1:10
+    ResultCIETData(:,i) = readValue(uaClient,DataNode)';
+    pause(1);
+    for j = 2:length(ResultCIETData(:,i))
+        plot(ResultCIETData(1,:),ResultCIETData(j,:));
+        hold on
+    end
+end
+
+    
