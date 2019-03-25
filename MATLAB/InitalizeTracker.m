c4cf2a23544b6c02ef1c594f71f13f24f51bd4e4
iankolaja <39423229+iankolaja@users.noreply.github.com>
2019-03-25 16:50:25 -0700

    Tweaked file names

--- MATLAB/InitalizeTracker.m
+++ MATLAB/InitalizeTracker.m
@@ -2,7 +2,7 @@ state = 1;
 time = 0;
 controlVars = 7:15;
 measureVars = 16:17;
-load("Plant Operation.mat");
+load("Plant Operation Headers.mat");
 stateScheme = GenProcedure("Plant Operation.csv",Headers)
 controlVars = controlVars - 1;
 measureVars = measureVars - 1;
